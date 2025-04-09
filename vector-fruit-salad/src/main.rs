56bb80b0770b3de1de4bd45c8cf48533b51d7da7
martin-maverick <97390728+djshampz904@users.noreply.github.com>
2025-04-09 12:27:46 +0000

    Add user input functionality to fruit salad program

--- vector-fruit-salad/src/main.rs
+++ vector-fruit-salad/src/main.rs
@@ -6,6 +6,7 @@ useful data structures in Rust. A vector is represented using the Vec<T> type.
 
 use rand::seq::SliceRandom; // rand is a random number generation library in Rust
 use rand::thread_rng;
+use std::io;
 
 fn main() {
     let mut fruit = vec![
@@ -17,10 +18,46 @@ fn main() {
         "Pear",
         "Peach",
     ];
+    let mut rng = thread_rng();
+
+    let mut fruit_2 = vec!["Mango", "Banana", "Grapes", "Oranges", "Strawberry"];
+
+    println!("Please enter the name of the fruit you want to add to the basket: ");
+    let mut user_input = String::new();
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Failed to read line");
+
+    println!("Please enter the number of fruits you want to add to the basket: (maximum 3)");
+    let mut user_input_2 = String::new();
+    io::stdin()
+        .read_line(&mut user_input_2)
+        .expect("Failed to read line");
+    let user_input_2: i32 = user_input_2.trim().parse().expect("Please enter a number!");
+
+    if user_input_2 > 3 {
+        println!("You can only add a maximum of 3 fruits.");
+        return;
+    }
+
+    let random_fruits: Vec<_> = fruit_2
+        .choose_multiple(&mut rng, user_input_2 as usize)
+        .collect();
+    println!("Randomly selected fruits: {:?}", random_fruits);
+
+    fruit.extend(random_fruits);
+
+    fruit.push(user_input.trim()); // Add user input to the fruit vector
 
     // Scramble (shuffle) the fruit
     let mut rng = thread_rng();
     fruit.shuffle(&mut rng);
+
+    if let Some(random_fruit) = fruit.choose(&mut rng) {
+        println!("Random fruit: {}", random_fruit);
+    } else {
+        println!("No fruit available.");
+    }
 
     // Print out the fruit salad
     println!("Fruit Salad:");
